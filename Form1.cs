b8ffee18f5401619e4c5f921717891fd1903c70e
Daniil Iantikov <45979470+Shpuunt1k@users.noreply.github.com>
2020-12-18 22:23:03 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,7 +13,6 @@ namespace WF_Kurs
     public partial class Form1 : Form
     {
         private ListOfContacts Contacts;
-
         public Form1()
         {
             InitializeComponent();
@@ -33,6 +32,8 @@ namespace WF_Kurs
         {
             if (textBoxNewNum.Text != "")
             {
+                Number number_buf = new Number(textBoxNewEmail.Text, textBoxNewEmail.Location);
+                Contacts.people[listBox1.SelectedIndex].Add_number(number_buf);
                 textBoxNewNum.Text = "Добавить номер...";
                 textBoxNewNum.Top += 11;
             }
@@ -54,6 +55,21 @@ namespace WF_Kurs
 
         private void buttonAdd_Click(object sender, EventArgs e)
         {
+            if (listBox1.SelectedIndex != -1)
+                Contacts.people[listBox1.SelectedIndex].NumbersVisibleSwitch();
+            textBoxName.ReadOnly = false;
+            textBoxName.Text = "";
+            textBoxAdress.ReadOnly = false;
+            textBoxAdress.Text = "";
+            textBoxDate.ReadOnly = false;
+            textBoxName.Text = "";
+            textBoxNewEmail.ReadOnly = false;
+            textBoxNewEmail.Top = 45;
+            textBoxNewEmail.Text = "";
+            textBoxNewNum.ReadOnly = false;
+            textBoxNewNum.Top = 76;
+            textBoxNewNum.Text = "";
+            buttonAcceptAdd.Visible = true;
         }
 
         private void buttonDelete_Click(object sender, EventArgs e)
@@ -66,6 +82,7 @@ namespace WF_Kurs
 
         private void buttonAcceptAdd_Click(object sender, EventArgs e)
         {
+            
         }
 
         private void buttonAcceptCh_Click(object sender, EventArgs e)
