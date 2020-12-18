74298f872c2b38c3f829a50915b3bde977fa8db0
Daniil Iantikov <45979470+Shpuunt1k@users.noreply.github.com>
2020-12-19 01:36:51 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -13,10 +13,13 @@ namespace WF_Kurs
     public partial class Form1 : Form
     {
         private ListOfContacts Contacts;
+        private bool Action; //true->изменение контакта, false-> добавление
+
         public Form1()
         {
             InitializeComponent();
             Contacts = new ListOfContacts();
+            Action = true;
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -25,15 +28,19 @@ namespace WF_Kurs
 
         private void textBoxNewNum_MouseClick(object sender, MouseEventArgs e)
         {
-            textBoxNewNum.Text = "";
+            if (!textBoxNewNum.ReadOnly)
+                textBoxNewNum.Text = "";
         }
 
         private void textBoxNewNum_TextChanged(object sender, EventArgs e)
         {
             if (textBoxNewNum.Text != "")
             {
-                Number number_buf = new Number(textBoxNewEmail.Text, textBoxNewEmail.Location);
-                Contacts.people[listBox1.SelectedIndex].Add_number(number_buf);
+                Number number_buf = new Number(textBoxNewNum.Text.Substring(0, 1), textBoxNewNum.Location, this);
+                if (Action)
+                    Contacts.people[listBox1.SelectedIndex].Add_number(number_buf);
+                else
+                    Contacts.people[Contacts.people.Count - 1].Add_number(number_buf);
                 textBoxNewNum.Text = "Добавить номер...";
                 textBoxNewNum.Top += 11;
             }
@@ -41,22 +48,30 @@ namespace WF_Kurs
 
         private void textBoxNewEmail_MouseClick(object sender, MouseEventArgs e)
         {
-            textBoxNewEmail.Text = "";
+            if (!textBoxNewEmail.ReadOnly)
+                textBoxNewEmail.Text = "";
         }
 
         private void textBoxNewEmail_TextChanged(object sender, EventArgs e)
         {
             if (textBoxNewEmail.Text != "")
             {
-                textBoxNewEmail.Text = "Добавить номер...";
+                Email email_buf = new Email(textBoxNewEmail.Text.Substring(0, 1), textBoxNewEmail.Location);
+                if (Action)
+                    Contacts.people[listBox1.SelectedIndex].Add_email(email_buf);
+                else
+                    Contacts.people[Contacts.people.Count].Add_email(email_buf);
+                textBoxNewEmail.Text = "Добавить Email...";
                 textBoxNewEmail.Top += 11;
             }
         }
 
         private void buttonAdd_Click(object sender, EventArgs e)
         {
+            Action = false;
+            Contacts.Add_new_contact("");
             if (listBox1.SelectedIndex != -1)
-                Contacts.people[listBox1.SelectedIndex].NumbersVisibleSwitch();
+                Contacts.people[listBox1.SelectedIndex].NumEmVisibleSwitch();
             textBoxName.ReadOnly = false;
             textBoxName.Text = "";
             textBoxAdress.ReadOnly = false;
@@ -64,11 +79,9 @@ namespace WF_Kurs
             textBoxDate.ReadOnly = false;
             textBoxName.Text = "";
             textBoxNewEmail.ReadOnly = false;
-            textBoxNewEmail.Top = 45;
-            textBoxNewEmail.Text = "";
+            textBoxNewEmail.Top = 34;
             textBoxNewNum.ReadOnly = false;
-            textBoxNewNum.Top = 76;
-            textBoxNewNum.Text = "";
+            textBoxNewNum.Top = 65;
             buttonAcceptAdd.Visible = true;
         }
 
@@ -82,7 +95,7 @@ namespace WF_Kurs
 
         private void buttonAcceptAdd_Click(object sender, EventArgs e)
         {
-            
+            Contacts.people[Contacts.people.Count - 1].NumEmVisibleSwitch();
         }
 
         private void buttonAcceptCh_Click(object sender, EventArgs e)
